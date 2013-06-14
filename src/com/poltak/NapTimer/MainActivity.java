45a8b90eeef50481088cb5e33d8b5ff107918eb8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2013-06-14 19:09:17 +1000

    Refactoring and minor updates in MainActivity class, updated README.

--- src/com/poltak/NapTimer/MainActivity.java
+++ src/com/poltak/NapTimer/MainActivity.java
@@ -1,51 +1,81 @@
 package com.poltak.NapTimer;
 
-import java.util.Calendar;
-
-import com.poltak.NapTimer.R;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity
 {
-	public final static String EXTRA_MINUTES = 
-			"com.poltak.NapTimer.MINUTES";
-	private final static int BAD_NUMBER = -1;
-	
+    private final static int BAD_NUMBER = -1;
+
+    private EditText userInput;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.user_input);
+        userInput.setRawInputType(Configuration.KEYBOARD_12KEY);    // Make numpad popup instead of QWERTY keyboard.
+
+        Button setButton = (Button) findViewById(R.id.set_button);
+        setButton.setOnClickListener(new View.OnClickListener()     // Call setAlarm() on Button click.
+        {
+            @Override
+            public void onClick(View v) { setAlarm(); }
+        });
 	}
 
-	/** Called when the user clicks the Set button */
-	public void setAlarm(View view)
+    /** Starts alarm activity with a calculated time from the EditText's user specified minute offset. */
+	public void setAlarm()
 	{
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String inputStr = editText.getText().toString();
-		short minutes = (short) getMinutesFromString(inputStr);
-		
-		if ((minutes & 0x80) != 0)	// Number is negative (including BAD_NUMBER)
-			setHint(editText, R.string.bad_number_message);
-		else						// Number is positive
+		String   inputStr  = userInput.getText().toString();
+		int      minutes   = getMinutesFromString(inputStr);
+
+        userInput.setText("");                                      // Clear whatever text user has entered.
+
+		if (minutes < 0)    // Catches BAD_NUMBER and negatives.
+        {
+            userInput.setHint(R.string.bad_number_message);
+        } else
 		{
-			// Create new Intent for the alarm and start it
+			// Create new Intent for the alarm and start it.
 			Intent alarmIntent = getAlarmIntent(minutes);
 			startActivity(alarmIntent);
-			
-			// Remove input and reset edit text box to default hint
-			setHint(editText, R.string.edit_message);
+
+			// Reset edit text box to default hint.
+            userInput.setHint(R.string.edit_message);
 		}
 	}
-	
+
+	/**
+	 * Parses the input string to get it in integer form.
+	 * If a NumberFormatException is thrown, return BAD_NUMBER for processing in calling method.
+	 * @param input The input string the user enters.
+	 * @return The number of minutes specified as an integer.
+	 */
+	private int getMinutesFromString(String input)
+	{
+		int minutes;
+
+		try {
+			minutes = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			return BAD_NUMBER;
+		}
+
+		return minutes;
+	}
+
 	/**
 	 * Creates a new Intent for an alarm clock setting the hour and minute of
 	 * the day when it should ring, using values specified in the calculated
@@ -53,55 +83,22 @@ public class MainActivity extends Activity
 	 * @param offset The number of minutes to offset the alarm clock.
 	 * @return The alarm clock Intent set for the offset time.
 	 */
-	@SuppressLint("InlinedApi")
-	private Intent getAlarmIntent(short offset)
+	private Intent getAlarmIntent(int offset)
 	{
 		Intent alarm = new Intent(AlarmClock.ACTION_SET_ALARM);
-		
-		// Get current time and add to it the minute offset
+
+		// Get current time and add to it the minute offset.
 		Calendar offsetTime = Calendar.getInstance();
 		offsetTime.add(Calendar.MINUTE, offset);
-		
-		// Set the extra fields for the alarm based on new Calendar values
-		alarm.putExtra(
-				AlarmClock.EXTRA_HOUR, offsetTime.get(Calendar.HOUR_OF_DAY));
-		alarm.putExtra(
-				AlarmClock.EXTRA_MINUTES, offsetTime.get(Calendar.MINUTE));
-		
+
+		// Set the extra fields for the alarm based on new Calendar values.
+		alarm.putExtra(AlarmClock.EXTRA_HOUR,    offsetTime.get(Calendar.HOUR_OF_DAY));
+		alarm.putExtra(AlarmClock.EXTRA_MINUTES, offsetTime.get(Calendar.MINUTE));
+
+        // Specify to skip alarm app UI.
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
 			alarm.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
-		
+
 		return alarm;
-	}
-	
-	/**
-	 * Parses the input string to get it in integer form.
-	 * If a NumberFormatException is thrown, return BAD_NUMBER for processing
-	 * in calling method.
-	 * @param input The input string the user enters.
-	 * @return The number of minutes specified as an integer.
-	 */
-	private int getMinutesFromString(String input)
-	{
-		int minutes;
-		
-		try {
-			minutes = Integer.parseInt(input);
-		} catch (NumberFormatException e) {
-			return BAD_NUMBER;
-		}
-		
-		return minutes;
-	}
-	
-	/**
-	 * Clears the EditText box and sets the hint to @param editMessage.
-	 * @param editText EditText object to perform operation on.
-	 * @param newHint New hint to set. Need to specify path to XML string.
-	 */
-	private void setHint(EditText editText, int newHint)
-	{
-		editText.setText("");
-		editText.setHint(newHint);
 	}
 }
